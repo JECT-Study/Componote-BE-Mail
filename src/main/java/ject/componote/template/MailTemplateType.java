af8667bb0221844a468983dc33c0c865a4f42a9e
kmw2378 <rlarla677@gmail.com>
2025-01-16 13:36:29 +0900

    [FEAT] MailTemplateType 추가

---
+++ src/main/java/ject/componote/template/MailTemplateType.java
@@ -0,0 +1,17 @@
+package ject.componote.template;
+
+import lombok.Getter;
+
+@Getter
+public enum MailTemplateType {
+    COMMENT_LIKE("comment-like"),
+    ROOT_REPLY("root-reply"),
+    NOTICE("notice"),
+    NESTED_REPLY("nested-reply");
+
+    private final String templatePath;
+
+    MailTemplateType(final String templatePath) {
+        this.templatePath = templatePath;
+    }
+}
